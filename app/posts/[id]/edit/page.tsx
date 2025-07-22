bb7733c91a1c79e1033a45c6a4ebbd7eb3c47c73
0219Angry <angry.yuma.0219@gmail.com>
2025-07-22 20:26:22 +0900

    PagePropsの型定義を削除

--- app/posts/[id]/edit/page.tsx
+++ app/posts/[id]/edit/page.tsx
@@ -3,10 +3,6 @@ import { notFound } from 'next/navigation';
 import PostForm from '@/components/features/posts/PostForm';
 import { updatePost } from '@/lib/actions/postActions';
 
-type PageProps = {
-  params: { id: string };
-};
-
 export default async function EditPostPage({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
   const post = await prisma.post.findUnique({
