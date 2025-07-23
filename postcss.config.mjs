79757970857da0d91814b77cd05c0ec05fcc6a1b
0219Angry <angry.yuma.0219@gmail.com>
2025-07-23 21:04:59 +0900

    postcssを修正

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,5 @@ export default {
   plugins: {
     '@tailwindcss/postcss': {}, // v4からの新しいプラグイン
     'autoprefixer': {},        // 各ブラウザ用のベンダープレフィックスを自動で付与
-    '@tailwindcss/typography': {}, // Tailwind CSS Typography プラグイン
   },
 };
